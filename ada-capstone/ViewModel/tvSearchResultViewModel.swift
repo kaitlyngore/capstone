8d88d0afc21c24df3f33c415de4a11a50e59c708
kaitlyngore <kaitlyngore@gmail.com>
2022-02-03 14:39:57 -0500

    fix search

--- ada-capstone/ViewModel/tvSearchResultViewModel.swift
+++ ada-capstone/ViewModel/tvSearchResultViewModel.swift
@@ -7,19 +7,29 @@
 
 import Foundation
 import Combine
+
 @MainActor
     class TvSearchResultListViewModel: ObservableObject {
 
         @Published var searchResults: [TvSearchResultViewModel] = []
 
         func populateResults(searchText: String) async {
-            
+            var components = URLComponents()
+            components.scheme = "https"
+            components.host = "api.themoviedb.org"
+            components.path = "/3/search/tv"
+            let queryItemApiKey = URLQueryItem(name: "api_key", value: APIKey)
+
+            let queryItemQuery = URLQueryItem(name: "query", value: searchText)
+
+            components.queryItems = [queryItemApiKey, queryItemQuery]
+//            var defaultUrl = URL(string: "https://api.themoviedb.org/3/search/tv")
+
             do {
-                let searchResponse = try await callAPI<TvSearchResponse>().get(url: URL(string: "https://api.themoviedb.org/3/search/tv?api_key=\(APIKey)&language=en-US&page=1&query=\(searchText)&include_adult=false")!) { data in
-                    
-                    var str: String = String(data: data, encoding: .utf8)!
-                    
-                    
+
+                let searchResponse = try await callAPI<TvSearchResponse>().get(url: components.url!) { data in
+
+
                     return try? JSONDecoder().decode(TvSearchResponse.self, from: data)
                     }
 
@@ -34,6 +44,34 @@ import Combine
         }
 
     }
+
+//@MainActor
+//    class TvSearchResultListViewModel: ObservableObject {
+//
+//        @Published var searchResults: [TvSearchResultViewModel] = []
+//
+//        func populateResults(searchText: String) async {
+//
+//            do {
+//                let searchResponse = try await callAPI<TvSearchResponse>().get(url: URL(string: "https://api.themoviedb.org/3/search/tv?api_key=\(APIKey)&language=en-US&page=1&query=\(searchText)&include_adult=false")!) { data in
+//
+//                    var str: String = String(data: data, encoding: .utf8)!
+//
+//
+//                    return try? JSONDecoder().decode(TvSearchResponse.self, from: data)
+//                    }
+//
+//                DispatchQueue.main.async {self.searchResults =
+//                searchResponse.results.map(TvSearchResultViewModel.init)
+//                }
+//
+//            } catch {
+//                print(error)
+//            }
+//
+//        }
+//
+//    }
 
     struct TvSearchResultViewModel: Identifiable {
 
@@ -68,41 +106,3 @@ import Combine
 
 
 ////try to use url components
-//@MainActor
-//    class TvSearchResultListViewModel: ObservableObject {
-//
-//        @Published var searchResults: [TvSearchResultViewModel] = []
-//
-//        func populateResults(searchText: String) async {
-//            var components = URLComponents()
-//            components.scheme = "https"
-//            components.host = "api.themoviedb.org"
-//            components.path = "/3/search/tv"
-//            let queryItemApiKey = URLQueryItem(name: "api_key", value: APIKey)
-//
-//            let queryItemQuery = URLQueryItem(name: "query", value: searchText)
-//
-//            components.queryItems = [queryItemApiKey, queryItemQuery]
-//            var defaultUrl = URL(string: "https://api.themoviedb.org/3/search/tv")
-//
-//            do {
-//                var finalUrl = try String(contentsOf: (components.url ?? defaultUrl)!)
-//
-//                let searchResponse = try await callAPI<TvSearchResponse>().get(url: URL(string: finalUrl)!) { data in
-//
-////                    var str: String = String(data: data, encoding: .utf8)!
-//
-//                    return try? JSONDecoder().decode(TvSearchResponse.self, from: data)
-//                    }
-//
-//                DispatchQueue.main.async {self.searchResults =
-//                searchResponse.results.map(TvSearchResultViewModel.init)
-//                }
-//
-//            } catch {
-//                print(error)
-//            }
-//
-//        }
-//
-//    }
--- ada-capstone/Views/SearchResultCellView.swift
+++ ada-capstone/Views/SearchResultCellView.swift
@@ -11,11 +11,15 @@ import SwiftUI
 struct SearchResultCellView: View {
 
     let searchResult: TvSearchResultViewModel
-
+//    var url = URL(string: "https://image.tmdb.org/t/p/w500\(searchResult.poster_path)")
     var body: some View {
         HStack(spacing: 20) {
             VStack {
+                AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(searchResult.poster_path)"))
+               
+            
                 HStack{
+                    
                 Text(searchResult.name)
                             .bold()
                     Spacer()

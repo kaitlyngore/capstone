c240b7f790b8868e2bcab92d2b3daaf1ad598b29
kaitlyngore <kaitlyngore@gmail.com>
2022-02-03 11:33:51 -0500

    add api call class

---
+++ ada-capstone/API/callAPI2.swift
@@ -0,0 +1,41 @@
+//
+//  callAPI2.swift
+//  ada-capstone
+//
+//  Created by Kaitlyn Gore on 2/2/22.
+//
+
+import Foundation
+
+
+//
+//enum NetworkError: Error {
+//    case badRequest
+//    case decodingError
+//}
+//
+
+enum NetworkError: Error {
+    case badRequest
+    case decodingError
+}
+
+class callAPI<T: Decodable> {
+//    performing on a type which is decodable - need to create those types
+//
+    func get(url: URL, parse: (Data) -> T?) async throws -> T {
+            
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            if (response as? HTTPURLResponse)?.statusCode != 200 {
+                throw NetworkError.badRequest
+            }
+            
+            guard let result = parse(data) else {
+                throw NetworkError.decodingError
+            }
+            
+            return result
+        }
+        
+    }

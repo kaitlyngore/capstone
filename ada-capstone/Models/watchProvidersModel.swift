f7c302071c17e668a04e5e0821c115c3b65f546e
kaitlyngore <kaitlyngore@gmail.com>
2022-02-10 13:25:51 -0500

    add watchProviders model

---
+++ ada-capstone/Models/watchProvidersModel.swift
@@ -0,0 +1,36 @@
+//
+//  watchProvidersModel.swift
+//  ada-capstone
+//
+//  Created by Kaitlyn Gore on 2/9/22.
+//
+
+import Foundation
+
+struct WatchProviderResponse: Decodable {
+    let results: Results
+}
+
+struct Results: Decodable {
+    let US: CountryCode
+}
+
+struct CountryCode: Decodable {
+    let link: String
+    let flatrate: [FlatrateItem]
+    let buy: [BuyItem]?
+}
+
+struct FlatrateItem: Decodable, Identifiable {
+    let display_priority: Int
+    let logo_path: String?
+    let provider_name: String
+    let provider_id: Int
+    var id: Int{provider_id}
+}
+
+struct BuyItem: Decodable {
+    let display_priority: Int
+    let logo_path: String?
+    let provider_name: String
+}

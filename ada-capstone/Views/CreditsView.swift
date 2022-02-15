885a95a83373cb73a6961bdb796771dfabe3d829
kaitlyngore <kaitlyngore@gmail.com>
2022-02-15 17:06:39 -0500

    updates creditsView

--- ada-capstone/Views/CreditsView.swift
+++ ada-capstone/Views/CreditsView.swift
@@ -16,40 +16,44 @@ struct CreditsView: View {
     var body: some View {
         NavigationView {
             VStack{
-                
-                Spacer()
-                
-                Text("Show Data:")
-                    .font(.title)
-                    .fontWeight(.bold)
-                //                .tracking(2)
-                
-                Image("tmdb_logo")
+                Image("ellipsisLogo")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(maxWidth: 300, maxHeight: 50)
-                
-                Text("Streaming Data:")
-                    .font(.title)
-                    .fontWeight(.bold)
-                
-                
-                Image("JustWatchLogo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxWidth: 300, maxHeight: 50)
-                Text("Thanks To:")
-                    .font(.title)
-                    .fontWeight(.bold)
-                
-                Spacer()
+                    .frame(minWidth: 200, maxWidth: 300, minHeight: 75)
+                Text("By: Kaitlyn Gore")
+                    .font(.headline)
                 Spacer()
                 
+                Group {
+                    Text("Show Data:")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    //                .tracking(2)
+                    
+                    Image("tmdb_logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 200, maxHeight: 50)
+                    Text("This product uses the TMDB API but is not endorsed or certified by TMDB.")
+                    Text("")
+                    Text("Streaming Data:")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    
+                    Image("JustWatchLogo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 200, maxHeight: 50)
+                    
+                }
+                Spacer()
+                Spacer()
             }
             .navigationTitle("Credits")
             .navigationBarTitle(Text("Notifications"), displayMode: .inline)
             .navigationBarItems(trailing:
-                Button(action: {
+                                    Button(action: {
                 self.showCreditsView = false
             }){
                 Text("Done").bold()
@@ -58,10 +62,3 @@ struct CreditsView: View {
         
     }
 }
-
-
-//struct CreditsView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        CreditsView(showCreditsView: $showCreditsView)
-//    }
-//}
